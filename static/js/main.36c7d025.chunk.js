f720550ee5f468861a3f9e81a55d16eb3f38a9a0
chillhaze <alexander.gotra@gmail.com>
2021-09-02 22:34:50 +0300

    Updates

---
+++ static/js/main.36c7d025.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),o=c.n(i),s=(c(8),c(0)),a=function(){return Object(s.jsx)("div",{})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.36c7d025.chunk.js.map
